a03577c062f31a027d10531f58bd4c0435824093
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-09-26 02:18:31 +0000

    Practice Complete

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let name = "Rebekah";
+  let age = 80;
+
+  // conditional rendering
+  // if ...
+  // render this
+  // else
+  // render that
+
+  let string = `<div>My name is ${name}. I am ${age} years old.</div>`;
+  let canVote = ``;
+
+  canVote =
+    age >= 18
+      ? `<div>I will be voting in November.</div>`
+      : `<div>I am too young to vote.</div>`;
+
+  document.querySelector(".demo").innerHTML = `${string} ${canVote}`;
 };
